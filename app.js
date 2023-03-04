366e6e715cedc538f2e9eaf8a0acedcb8a16c7dd
Samoina <samoina.w@gmail.com>
2023-03-04 14:08:36 +0300

    Use throttle function to limit function calls during scroll down

--- app.js
+++ app.js
@@ -42,9 +42,36 @@ const manageMenuOnScrollDown= ()=>{
 WITHOUT THROTTLE
 This really confused me as to when to call the lfunction to manage scrolldown, i then realized i need to set an EL onto the window object to listen for scrolling
 */
-window.addEventListener('scroll', manageMenuOnScrollDown);
+// window.addEventListener('scroll', manageMenuOnScrollDown);
 
+/*
+with scroll event listeners, I learnt about throttle as a way to minimize the number of times a function is called for better performance. This, as opposed to calling the function every time a pixel is moved. A throttle function is a higher order function that acts as a timer for the function passed into it. Steps to creating a throttle function
+1. initialize a throttlePause variable
+2. create a throttle function expression that takes in a callback, in this case, our manageMenuOnScrollDown function, and time
+3. if the throttlewait variable is true, do not run the function
+4. set the variable to true to pause the function
+5. Add settimeout to run the function in th time specified
+6. set the throttleWait variable to false after the timer to restart the function
+*/
 
+//WITH THROTTLE
+
+var throttlePause;
+
+const throttle = (callback, time) =>{
+  if(throttlePause) return;
+
+  throttlePause = true;
+
+  setTimeout(() => {
+    callback();
+    throttlePause = false;
+  }, time);
+}
+
+window.addEventListener('scroll', ()=>{
+  throttle(manageMenuOnScrollDown, 250)
+});
 
 
 
